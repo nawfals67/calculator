d1c416e8372c8a4a4a9fa311ee9bff42a5c648bd
nawfals67 <67345177+nawfals67@users.noreply.github.com>
2020-06-23 15:04:18 -0700

    Update helpers.js

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,7 @@
 var supertest = require('supertest');
 var chai = require('chai');
 var app = require('../server');
-
+// another comment 2
 global.app = app;
 global.expect = chai.expect;
 global.request = supertest(app);
